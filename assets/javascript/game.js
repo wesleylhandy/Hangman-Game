686983a3169cd05a737b6d39282d78e492bca9be
Wesley Handy <wesleylhandy@gmail.com>
2016-11-27 23:39:39 -0500

    will need to add new logic for mobile keyboard events, for now, reverting to desktop only

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -304,12 +304,16 @@ var hangmanGame = {
 
 		if (this.usedGuesses >= this.maxGuesses) {
 			this.gameOn = false;
+			
+			/* -- for mobile and tab keyboards, needs work
 			document.activeElement.blur();
+			-- */
+			
 			this.losses++;
 
 			//display you lose and current term
 			document.querySelector(".alerts").innerHTML = "Sorry, you lose. The correct answer was: " + this.currentTerm.word;
-			//display used terms - in process
+			// --- display used terms - in process
 
 			//play losing sound
 			var id1 = this.sounds.lose.sound.play();
@@ -319,12 +323,16 @@ var hangmanGame = {
 			document.querySelector("#startButton").innerHTML = "Play Again?";
 		} else if (blanksCheck === lettersCheck) {
 			this.gameOn = false;
-			document.activeElement.blur();
+			
+			/* -- for mobile and tab keyboards, needs work
+			//document.activeElement.blur();
+			-- */
+
 			this.wins++;
 
 			//display you win with winning sound/image
 			document.querySelector(".alerts").innerHTML = "Good Job. You got it right!";
-			//display used terms - in process
+			// --- display used terms - in process
 
 			//play winning sound
 			var id1 = this.sounds.win.sound.play();
@@ -355,7 +363,13 @@ var startGame = function() {
 	hangmanGame.reset();
 	hangmanGame.gameOn = true;
 	hangmanGame.getRandomTerm();
+
+	/*
+		Will need better solution for mobile and tablet keyboards
+		since keycodes are device specific come back to this
 	document.getElementById("dummy").focus();
+	*/
+
 	//use querySelector to change content of start button to Start Over
 	document.querySelector("#startButton").innerHTML = "Start Over";
 }
@@ -366,7 +380,32 @@ document.onkeyup = function(event) {
 	if (hangmanGame.gameOn) {
 		
   		hangmanGame.userGuess = event.key;
-  		//only allow alphabetical keys for type = txt
+
+  		/* to get to read mobile keyboards, will have to use
+  		a solution comparing elements to an array of acceptable
+  		keys, such as the following (using jQuery)
+  		see: http://stackoverflow.com/questions/31979619/how-to-get-keycodes-from-android-mobile-device-keyboard
+  		and: http://stackoverflow.com/questions/302122/jquery-event-keypress-which-key-was-pressed
+
+  			//keyboard maping array   
+			var keyboardMap = ["", "", "", "CANCEL", "", "", "HELP", "", "BACK_SPACE", "TAB", "", "", "CLEAR", "ENTER", "RETURN", "", "SHIFT", "CONTROL", "ALT", "PAUSE", "CAPS_LOCK", "KANA", "EISU", "JUNJA", "FINAL", "HANJA", "", "ESCAPE", "CONVERT", "NONCONVERT", "ACCEPT", "MODECHANGE", "SPACE", "PAGE_UP", "PAGE_DOWN", "END", "HOME", "LEFT", "UP", "RIGHT", "DOWN", "SELECT", "PRINT", "EXECUTE", "PRINTSCREEN", "INSERT", "DELETE", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "COLON", "SEMICOLON", "LESS_THAN", "EQUALS", "GREATER_THAN", "QUESTION_MARK", "AT", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "WIN", "", "CONTEXT_MENU", "", "SLEEP", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "MULTIPLY", "ADD", "SEPARATOR", "SUBTRACT", "DECIMAL", "DIVIDE", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "", "NUM_LOCK", "SCROLL_LOCK", "WIN_OEM_FJ_JISHO", "WIN_OEM_FJ_MASSHOU", "WIN_OEM_FJ_TOUROKU", "WIN_OEM_FJ_LOYA", "WIN_OEM_FJ_ROYA", "", "", "", "", "", "", "", "", "", "CIRCUMFLEX", "EXCLAMATION", "DOUBLE_QUOTE", "HASH", "DOLLAR", "PERCENT", "AMPERSAND", "UNDERSCORE", "OPEN_PAREN", "CLOSE_PAREN", "ASTERISK", "PLUS", "PIPE", "HYPHEN_MINUS", "OPEN_CURLY_BRACKET", "CLOSE_CURLY_BRACKET", "TILDE", "", "", "", "", "VOLUME_MUTE", "VOLUME_DOWN", "VOLUME_UP", "", "", "", "", "COMMA", "", "PERIOD", "SLASH", "BACK_QUOTE", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "OPEN_BRACKET", "BACK_SLASH", "CLOSE_BRACKET", "QUOTE", "", "META", "ALTGR", "", "WIN_ICO_HELP", "WIN_ICO_00", "", "WIN_ICO_CLEAR", "", "", "WIN_OEM_RESET", "WIN_OEM_JUMP", "WIN_OEM_PA1", "WIN_OEM_PA2", "WIN_OEM_PA3", "WIN_OEM_WSCTRL", "WIN_OEM_CUSEL", "WIN_OEM_ATTN", "WIN_OEM_FINISH", "WIN_OEM_COPY", "WIN_OEM_AUTO", "WIN_OEM_ENLW", "WIN_OEM_BACKTAB", "ATTN", "CRSEL", "EXSEL", "EREOF", "PLAY", "ZOOM", "", "PA1", "WIN_OEM_CLEAR", ""];
+			//Allowed keys         
+			var allowedKeyNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "DELETE", "BACK_SPACE", "DECIMAL", "LEFT", "RIGHT", "TAB", "SUBTRACT", "PERIOD"];
+			var allowedKeyTxt = ...
+			//bind keydown to your element
+			element.on("keydown", function (e) {
+        		var key = e.charCode || e.keyCode || 0;
+        		var keyValue = keyboardMap[key];
+        		if ($.inArray(keyValue, allowedKey) !== -1){
+              		return true;
+        		}
+        		else{
+              		return  false;
+        		}
+			}
+		*/
+
+		//only allow alphabetical keys for type = txt
   		//only allow numerical keys for type = num
   		if ( (hangmanGame.currentTerm.type === "txt") 
   			&& ((event.keyCode > 64) && (event.keyCode < 91)) ) {
